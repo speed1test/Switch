b3d99d7192a87d2608ac73c11311b07627039fb5
Edwin <edwin12sv@gmail.com>
2023-12-25 19:52:37 -0600

    Initial Commit

---
+++ Switch/SwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitchApp.swift
+//  Switch
+//
+//  Created by elsalvador on 25/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
